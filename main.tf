df901e9f77b13e996314227cea54187f42872f56
unknown <ranjanipsa@gmail.com>
2021-07-01 22:44:43 +0530

    changes

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ resource "aws_lambda_function" "example" {
    function_name = "ServerlessExample"
 
    # The bucket name as created earlier with "aws s3api create-bucket"
-   #s3_bucket = var.s3_bucket
-   #s3_key    = "v1.0.0/example.zip"
+   s3_bucket = var.s3_bucket
+   s3_key    = example.zip
 
    # "main" is the filename within the zip file (main.js) and "handler"
    # is the name of the property under which the handler function was
